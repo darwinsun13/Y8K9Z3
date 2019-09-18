7ca4549442eb4adbdf55a6ebe38f9225dfdbbd1d
Darwin Sun <darwinsun13@gmail.com>
2019-09-19 00:40:43 +1200

    Added Coin Sprite and attemped to interact and generate coint (INCOMPLETE)

---
+++ Y8K9Z3/Assets/_Complete-Game/Scripts/Currency.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Currency : MonoBehaviour
+{
+
+    public int money;
+
+    GameObject currencyUI;
+    // Start is called before the first frame update
+    void Start()
+    {
+        currencyUI = GameObject.Find("Currency");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currencyUI.GetComponent<Text>().text = money.ToString();
+        if (money <= 0 || money == null)
+        {
+            money = 0;
+        }
+    }
+}
---
+++ Y8K9Z3/Assets/_Complete-Game/Scripts/PickupMoney.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickupMoney : MonoBehaviour
+{
+    // Start is called before the first frame update
+    Currency script;
+
+    public int amountAdded;
+
+    void Start()
+    {
+        script = GameObject.FindWithTag("GameController").GetComponent<Currency>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            script.money += amountAdded;
+            Destroy(gameObject);
+        }
+    }
+}
