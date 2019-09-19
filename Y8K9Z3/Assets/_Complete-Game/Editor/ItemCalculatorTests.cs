e86b96d04f8145988e04d5ec088e0936440687c1
playerfgfg <53203863+playerfgfg@users.noreply.github.com>
2019-09-19 21:46:50 +1200

    Editor for Unit Testing

---
+++ Y8K9Z3/Assets/_Complete-Game/Editor/ItemCalculatorTests.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using NUnit.Framework;
+
+public class ItemCalculatorTests
+{
+    [Test]
+    public void CalculateTotalPoints_Test()
+    {
+        // ARRANGE
+        var pointCalculator = new ItemCalculator();
+        var coinPoints = 100;
+        var potionPoints = 20;
+        var expectedPoints = (100 + 20);
+
+        // ACT
+        var points = pointCalculator.CalculateTotalPoints(coinPoints,potionPoints);
+        
+        // ASSERT
+        Assert.That(points, Is.EqualTo(expectedPoints));
+
+    }
+}
---
+++ Y8K9Z3/Assets/_Complete-Game/Scripts/ItemCalculator.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemCalculator 
+{
+    public int CalculateTotalPoints(
+        int potionPoints,
+        int coinPoints)
+    {
+        int points = 0;
+
+        points += potionPoints * 20;
+        points += coinPoints * 10;
+
+        return points; 
+
+    }
+
+}
