0a567dcd1e4371106a115717e967326ab68de62b
Jordie <jordiemuljana@gmail.com>
2019-09-20 02:08:29 +1200

    Added Unit Test for Attacks

---
+++ Y8K9Z3/Assets/_Complete-Game/Editor/ReceiveDamageTest.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using NUnit.Framework;
+
+public class ReceiveDamageTest
+{
+    [Test]
+    public void ReceiveDamage_Test()
+    {
+        // ARRANGE
+        //GameObject enemyGameObject = MonoBehaviour.Instantiate(Resources.Load<GameObject>("_Complete-Game/Scripts"));
+        //var enemy = enemyGameObject.GetComponent<Completed.Enemy>();
+        GameObject gameObject = new GameObject();
+        gameObject.AddComponent<Completed.Enemy>();
+        var enemy = gameObject.GetComponent<Completed.Enemy>();
+        var damage = 100;
+        var expected = false;
+
+        // ACT
+        enemy.ReceiveDamage(damage);
+        var enemyDeath = enemy.isAlive;
+
+        // ASSERT
+        Assert.That(expected, Is.EqualTo(enemyDeath));
+    }
+}
--- Y8K9Z3/Assets/_Complete-Game/Scripts/Enemy.cs
+++ Y8K9Z3/Assets/_Complete-Game/Scripts/Enemy.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 namespace Completed
 {
@@ -10,9 +11,9 @@ namespace Completed
         public int money;
         public int hp = 1;
 		public AudioClip attackSound1;						//First of two audio clips to play when attacking the player.
-		public AudioClip attackSound2;						//Second of two audio clips to play when attacking the player.
-		
-		
+		public AudioClip attackSound2;                      //Second of two audio clips to play when attacking the player.
+
+        public bool isAlive = true;
 		private Animator animator;							//Variable of type Animator to store a reference to the enemy's Animator component.
 		private Transform target;							//Transform to attempt to move toward each turn.
 		private bool skipMove;								//Boolean to determine whether or not enemy should skip a turn or move this turn.
@@ -104,12 +105,12 @@ namespace Completed
         {
             //enemy loses hp
             hp -= dmg;
-            Debug.Log("Enemy takes " + dmg + " damage.");
+            Debug.Log("Enemy takes " + dmg + " damage.");//for debug purposes
             //if hp is less than 0, destroy the enemy.
             if(hp <= 0)
             {
-
-                gameObject.SetActive(false);
+                isAlive = false;
+                this.gameObject.SetActive(false);
             }
         }
 	}
