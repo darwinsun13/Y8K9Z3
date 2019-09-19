4faa8a21bf203a937b89e030ec1177785cb091bc
Kooshan <55038456+akay4ty7@users.noreply.github.com>
2019-09-19 21:14:58 +1200

    Revert "a"
    This reverts commit 6debb499c3066b32e27593297719921de7669206, reversing
    changes made to 1f9bbcd3fa784a22bf32738763a7d481a5553f2d.

--- Y8K9Z3/Assets/_Complete-Game/Scripts/Player.cs
+++ Y8K9Z3/Assets/_Complete-Game/Scripts/Player.cs
@@ -11,16 +11,13 @@ namespace Completed
 		public float restartLevelDelay = 1f;		//Delay time in seconds to restart level.
 		public int pointsPerFood = 10;				//Number of points to add to player food points when picking up a food object.
 		public int pointsPerSoda = 20;				//Number of points to add to player food points when picking up a soda object.
-		public int wallDamage = 1;					//How much damage a player does to a wall when chopping it.
-		public int pointsPerCoin = 10;				//Number of points to add to player food points when picking up a food object.
-		public int pointsPerPotion = 20;				//Number of points to add to player food points when picking up a soda object.
-		public int wallDamage = 1;					//How much damage a player does to a wall when chopping it.
+		//public int wallDamage = 1;					//How much damage a player does to a wall when chopping it.
         public int attackDamage = 1;                //How much damage a player does to an enemy when attacking it.
 		public Text foodText;						//UI Text to display current player food total.
         public int direction = 0;                   //Direction the player is facing.
 
 		private Animator animator;					//Used to store a reference to the Player's animator component.
-		private int coin;                           //Used to store player food points total during level.
+		private int food;                           //Used to store player food points total during level.
         private int money;
 #if UNITY_IOS || UNITY_ANDROID || UNITY_WP8 || UNITY_IPHONE
         private Vector2 touchOrigin = -Vector2.one;	//Used to store location of screen touch origin for mobile controls.
@@ -34,10 +31,10 @@ namespace Completed
 			animator = GetComponent<Animator>();
 			
 			//Get the current food point total stored in GameManager.instance between levels.
-			coin = GameManager.instance.playerFoodPoints;
+			food = GameManager.instance.playerFoodPoints;
 			
 			//Set the foodText to reflect the current player food total.
-			foodText.text = "Hp: " + coin;
+			foodText.text = "Currency: " + food;
 			
 			//Call the Start function of the MovingObject base class.
 			base.Start ();
@@ -48,7 +45,7 @@ namespace Completed
 		private void OnDisable ()
 		{
 			//When Player object is disabled, store the current local food total in the GameManager so it can be re-loaded in next level.
-			GameManager.instance.playerFoodPoints = coin;
+			GameManager.instance.playerFoodPoints = food;
 		}
 		
 		
@@ -127,10 +124,10 @@ namespace Completed
 		//AttemptMove takes a generic parameter T which for Player will be of the type Wall, it also takes integers for x and y direction to move in.
 		protected override void AttemptMove <T> (int xDir, int yDir)
 		{
-            coin--;
+            food--;
 
 			//Update food text display to reflect current score.
-			foodText.text = "Hp: " + coin;
+			foodText.text = "Currency: " + food;
 			
 			//Call the AttemptMove method of the base class, passing in the component T (in this case Wall) and x and y direction to move.
 			base.AttemptMove <T> (xDir, yDir);
@@ -161,34 +158,52 @@ namespace Completed
             
             return hit;
         }
-
-
-        private void OnTriggerEnter2D(Collider2D other)
-        {
-            if (other.tag == "Staircase")
-            {
-                Invoke("Restart", restartLevelDelay);
-                enabled = false;
-            }
-
-            else if (other.tag == "Potion")
-            {
-                coin += pointsPerPotion;
-                foodText.text = "+" + pointsPerPotion + " Hp: " + coin;
-                other.gameObject.SetActive(false);
-            }
-
-            else if (other.tag == "Coin")
-            {
-                coin += pointsPerCoin;
-                foodText.text = "+" + pointsPerCoin + " Hp: " + coin;
-                other.gameObject.SetActive(false);
-            }
-        }
-
-
-        //Restart reloads the scene when called.
-        private void Restart ()
+		
+		
+		//OnTriggerEnter2D is sent when another object enters a trigger collider attached to this object (2D physics only).
+		private void OnTriggerEnter2D (Collider2D other)
+		{
+			//Check if the tag of the trigger collided with is Exit.
+			if(other.tag == "Exit")
+			{
+				//Invoke the Restart function to start the next level with a delay of restartLevelDelay (default 1 second).
+				Invoke ("Restart", restartLevelDelay);
+				
+				//Disable the player object since level is over.
+				enabled = false;
+			}
+			
+			//Check if the tag of the trigger collided with is Food.
+			else if(other.tag == "Hp")
+			{
+				//Add pointsPerFood to the players current food total.
+				food += pointsPerFood;
+				
+				//Update foodText to represent current total and notify player that they gained points
+				foodText.text = "+" + pointsPerFood + " Currency: " + food;
+				
+				
+				//Disable the food object the player collided with.
+				other.gameObject.SetActive (false);
+			}
+			
+			//Check if the tag of the trigger collided with is Soda.
+			else if(other.tag == "Soda")
+			{
+				//Add pointsPerSoda to players food points total
+				food += pointsPerSoda;
+				
+				//Update foodText to represent current total and notify player that they gained points
+				foodText.text = "+" + pointsPerSoda + " Currency: " + food;
+			
+				//Disable the soda object the player collided with.
+				other.gameObject.SetActive (false);
+			}
+		}
+		
+		
+		//Restart reloads the scene when called.
+		private void Restart ()
 		{
 			//Load the last scene loaded, in this case Main, the only scene in the game. And we load it in "Single" mode so it replace the existing one
             //and not load all the scene object in the current scene.
