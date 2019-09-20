78cb84cdc756543280125fe7ce464ef9352f6d5b
Jordie <jordiemuljana@gmail.com>
2019-09-20 22:30:54 +1200

    Cleaned Up Code
    Cleaned up comments and some code in Enemy.cs and Player.cs

--- Y8K9Z3/Assets/_Complete-Game/Scripts/Player.cs
+++ Y8K9Z3/Assets/_Complete-Game/Scripts/Player.cs
@@ -1,150 +1,92 @@
 ﻿using UnityEngine;
 using System.Collections;
-using UnityEngine.UI;	//Allows us to use UI.
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 namespace Completed
 {
-	//Player inherits from MovingObject, our base class for objects that can move, Enemy also inherits from this.
 	public class Player : MovingObject
 	{
-		public float restartLevelDelay = 1f;		//Delay time in seconds to restart level.
-		public int pointsPerFood = 10;				//Number of points to add to player food points when picking up a food object.
-		public int pointsPerSoda = 20;				//Number of points to add to player food points when picking up a soda object.
-		public int wallDamage = 1;					//How much damage a player does to a wall when chopping it.
-		public int pointsPerCoin = 10;				//Number of points to add to player food points when picking up a food object.
-		public int pointsPerPotion = 20;				//Number of points to add to player food points when picking up a soda object.
-		//public int wallDamage = 1;					//How much damage a player does to a wall when chopping it.
-        public int attackDamage = 1;                //How much damage a player does to an enemy when attacking it.
-		public Text foodText;						//UI Text to display current player food total.
-        public int direction = 0;                   //Direction the player is facing.
+		public float restartLevelDelay = 1f;		
+		public int pointsPerFood = 10;				
+		public int pointsPerSoda = 20;				
+		public int wallDamage = 1;					
+		public int pointsPerCoin = 10;				
+		public int pointsPerPotion = 20;				
+		//public int wallDamage = 1;					
+        public int attackDamage = 1;                
+		public Text foodText;						
+        public int direction = 0;                   
 
         public bool isPickedUp = true;
-        private Animator animator;					//Used to store a reference to the Player's animator component.
-		private int coin;                           //Used to store player food points total during level.
+        private Animator animator;					
+		private int coin;                           
         private int money;
 #if UNITY_IOS || UNITY_ANDROID || UNITY_WP8 || UNITY_IPHONE
         private Vector2 touchOrigin = -Vector2.one;	//Used to store location of screen touch origin for mobile controls.
 #endif
 		
 		
-		//Start overrides the Start function of MovingObject
 		protected override void Start ()
 		{
-			//Get a component reference to the Player's animator component
 			animator = GetComponent<Animator>();
-			
-			//Get the current food point total stored in GameManager.instance between levels.
+
 			coin = GameManager.instance.playerFoodPoints;
-			
-			//Set the foodText to reflect the current player food total.
+
 			foodText.text = "Coin: " + coin;
-			
-			//Call the Start function of the MovingObject base class.
+
 			base.Start ();
 		}
 		
 		
-		//This function is called when the behaviour becomes disabled or inactive.
 		private void OnDisable ()
 		{
-			//When Player object is disabled, store the current local food total in the GameManager so it can be re-loaded in next level.
 			GameManager.instance.playerFoodPoints = coin;
 		}
 		
 		
 		private void Update ()
 		{
-			//If it's not the player's turn, exit the function.
 			if(!GameManager.instance.playersTurn) return;
 			
-			int horizontal = 0;  	//Used to store the horizontal move direction.
-			int vertical = 0;		//Used to store the vertical move direction.
+			int horizontal = 0;  	
+			int vertical = 0;		
 			
 			//Check if we are running either in the Unity editor or in a standalone build.
 #if UNITY_STANDALONE || UNITY_WEBPLAYER
 			
-			//Get input from the input manager, round it to an integer and store in horizontal to set x axis move direction
 			horizontal = (int) (Input.GetAxisRaw ("Horizontal"));
 			
-			//Get input from the input manager, round it to an integer and store in vertical to set y axis move direction
 			vertical = (int) (Input.GetAxisRaw ("Vertical"));
 			
-			//Check if moving horizontally, if so set vertical to zero.
+
 			if(horizontal != 0)
 			{
 				vertical = 0;
 			}
-			//Check if we are running on iOS, Android, Windows Phone 8 or Unity iPhone
-#elif UNITY_IOS || UNITY_ANDROID || UNITY_WP8 || UNITY_IPHONE
-			
-			//Check if Input has registered more than zero touches
-			if (Input.touchCount > 0)
-			{
-				//Store the first touch detected.
-				Touch myTouch = Input.touches[0];
-				
-				//Check if the phase of that touch equals Began
-				if (myTouch.phase == TouchPhase.Began)
-				{
-					//If so, set touchOrigin to the position of that touch
-					touchOrigin = myTouch.position;
-				}
-				
-				//If the touch phase is not Began, and instead is equal to Ended and the x of touchOrigin is greater or equal to zero:
-				else if (myTouch.phase == TouchPhase.Ended && touchOrigin.x >= 0)
-				{
-					//Set touchEnd to equal the position of this touch
-					Vector2 touchEnd = myTouch.position;
-					
-					//Calculate the difference between the beginning and end of the touch on the x axis.
-					float x = touchEnd.x - touchOrigin.x;
-					
-					//Calculate the difference between the beginning and end of the touch on the y axis.
-					float y = touchEnd.y - touchOrigin.y;
-					
-					//Set touchOrigin.x to -1 so that our else if statement will evaluate false and not repeat immediately.
-					touchOrigin.x = -1;
-					
-					//Check if the difference along the x axis is greater than the difference along the y axis.
-					if (Mathf.Abs(x) > Mathf.Abs(y))
-						//If x is greater than zero, set horizontal to 1, otherwise set it to -1
-						horizontal = x > 0 ? 1 : -1;
-					else
-						//If y is greater than zero, set horizontal to 1, otherwise set it to -1
-						vertical = y > 0 ? 1 : -1;
-				}
-			}
-			
-#endif //End of mobile platform dependendent compilation section started above with #elif
-			//Check if we have a non-zero value for horizontal or vertical
+
+#endif
 			if(horizontal != 0 || vertical != 0)
 			{
 				AttemptMove<Enemy> (horizontal, vertical);
 			}
 		}
 		
-		//AttemptMove overrides the AttemptMove function in the base class MovingObject
-		//AttemptMove takes a generic parameter T which for Player will be of the type Wall, it also takes integers for x and y direction to move in.
+
 		protected override void AttemptMove <T> (int xDir, int yDir)
 		{
             coin--;
 
-			//Update food text display to reflect current score.
 			foodText.text = "Coin: " + coin;
-			
-			//Call the AttemptMove method of the base class, passing in the component T (in this case Wall) and x and y direction to move.
+
 			base.AttemptMove <T> (xDir, yDir);
-			
-			//Hit allows us to reference the result of the Linecast done in Move.
+
 			RaycastHit2D hit;
 
-			//Set the playersTurn boolean of GameManager to false now that players turn is over.
 			GameManager.instance.playersTurn = false;
 		}
 		
-		
-		//OnCantMove overrides the abstract function OnCantMove in MovingObject.
+
 		protected override void OnCantMove <T> (T component)
 		{
             Enemy enemy = component as Enemy;
@@ -189,11 +131,8 @@ namespace Completed
         }
 
 
-        //Restart reloads the scene when called.
         private void Restart ()
 		{
-			//Load the last scene loaded, in this case Main, the only scene in the game. And we load it in "Single" mode so it replace the existing one
-            //and not load all the scene object in the current scene.
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex, LoadSceneMode.Single);
 		}
 		
