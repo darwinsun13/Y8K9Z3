772736c8cbedff05451eb82a7a58014671441cd2
Max <max@MaxdeMacBook-Pro-2.local>
2019-09-20 14:37:08 +1200

    movement unit test

---
+++ Y8K9Z3/Assets/_Complete-Game/Editor/PlayerMovingTest.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using NUnit.Framework;
+
+public class PlayerMovingTest
+{
+    [Test]
+    public void PlayerMoving_Test_ShouldReturnTrue()
+    {
+        var result = PlayerMoving_Test(true);
+        Assert.IsTrue(result);
+    }
+
+    [Test]
+    public void PlayerMoving_Test_ShouldReturnFalse()
+    {
+        var result = PlayerMoving_Test(false);
+        Assert.IsFalse(result);
+    }
+
+    private bool PlayerMoving_Test(bool expectedMovingValidality)
+    {
+        bool result = true;
+
+        return result;
+    }
+}
