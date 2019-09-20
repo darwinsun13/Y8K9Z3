8bb72cd75bb3764060d160554cea51c14c5822b8
Darwin Sun <darwinsun13@gmail.com>
2019-09-20 22:20:24 +1200

    AddMoneyTest

---
+++ Y8K9Z3/Assets/_Complete-Game/Editor/AddMoneyTest.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using NUnit.Framework;
+
+public class AddMoneyTest : MonoBehaviour
+{
+    
+    [Test]
+    public void AddMoney_Test()
+    {
+        //Arrange (Default value player is set when loaded into game)
+        var coin = 0;
+        var expectedCoin = 10;
+
+        //ACT (When player walks over a coin)
+        coin += 10;
+
+        //Assert 
+        Assert.That(coin,Is.EqualTo(expectedCoin));
+    }
+
+    [Test]
+    public void AddMoney_Fail()
+    {
+        //Arrange (Default value player is set when loaded into game)
+        var coin = 0;
+        var expectedCoin = 10;
+
+        //ACT (When player walks over a coin)
+        coin += 1;
+
+        //Assert
+        Assert.That(coin, Is.EqualTo(expectedCoin));
+    }
+}
