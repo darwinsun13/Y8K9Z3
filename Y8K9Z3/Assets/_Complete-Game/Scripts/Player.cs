861e3affeba7abe65635a2092358ab8ac1c425cd
Jordie <jordiemuljana@gmail.com>
2019-10-09 16:53:09 +1300

    Update Player.cs
    Added some attributes (str, con, dex), tidied up some spacing.

--- Y8K9Z3/Assets/_Complete-Game/Scripts/Player.cs
+++ Y8K9Z3/Assets/_Complete-Game/Scripts/Player.cs
@@ -12,15 +12,17 @@ namespace Completed
 		public int pointsPerSoda = 20;				
 		public int wallDamage = 1;					
 		public int pointsPerCoin = 10;				
-		public int pointsPerPotion = 20;				
-		//public int wallDamage = 1;					
+		public int pointsPerPotion = 20;								
         public int attackDamage = 1;                
 		public Text foodText;						
-        public int direction = 0;                   
+        public int direction = 0;
+        public int str = 1;
+        public int con = 1;
+        public int dex = 1;
 
         public bool isPickedUp = true;
-        private Animator animator;					
-		private int coin;                           
+        private Animator animator;
+		private int coin;                  
         private int money;
 #if UNITY_IOS || UNITY_ANDROID || UNITY_WP8 || UNITY_IPHONE
         private Vector2 touchOrigin = -Vector2.one;	//Used to store location of screen touch origin for mobile controls.
