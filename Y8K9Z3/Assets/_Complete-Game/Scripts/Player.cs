b7136658accbb279e962ca92080191ba95d4ac51
playerfgfg <53203863+playerfgfg@users.noreply.github.com>
2019-10-20 17:12:11 +1300

    Further progress on inventory

--- Y8K9Z3/Assets/_Complete-Game/Scripts/Player.cs
+++ Y8K9Z3/Assets/_Complete-Game/Scripts/Player.cs
@@ -25,6 +25,9 @@ namespace Completed
         private Animator animator;
 		private int health;                  
         private int money = 0;
+        private Inventory inventory;
+        public GameObject itemButton;
+
 #if UNITY_IOS || UNITY_ANDROID || UNITY_WP8 || UNITY_IPHONE
         private Vector2 touchOrigin = -Vector2.one;	//Used to store location of screen touch origin for mobile controls.
 #endif
@@ -40,6 +43,11 @@ namespace Completed
 
 			base.Start ();
 		}
+
+        private void start()
+        {
+            inventory = GameObject.FindGameObjectWithTag("Player").GetComponent<Inventory>();
+        }
 		
 		
 		private void OnDisable ()
@@ -119,6 +127,18 @@ namespace Completed
 
             else if (other.tag == "Potion")
             {
+                //for (int i = 0; i < inventory.slots.Length; i++)
+                //{
+                //    if (inventory.isFull[i] == false)
+                //    {
+                //        inventory.isFull[i] = true;
+                //        Instantiate(itemButton, inventory.slots[i].transform, false);
+                //        isPickedUp = false;
+                //        other.gameObject.SetActive(false);
+                //        break;
+                //    }
+                //}
+
                 health += pointsPerPotion;
                 foodText.text = "+" + pointsPerPotion + " Health: " + health;
                 isPickedUp = false;
