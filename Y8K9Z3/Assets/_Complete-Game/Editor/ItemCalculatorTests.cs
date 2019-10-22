5ff940416c459747321356f00ee11031755696d6
playerfgfg <53203863+playerfgfg@users.noreply.github.com>
2019-10-22 16:57:55 +1300

    Unit tests

--- Y8K9Z3/Assets/_Complete-Game/Editor/ItemCalculatorTests.cs
+++ Y8K9Z3/Assets/_Complete-Game/Editor/ItemCalculatorTests.cs
@@ -22,22 +22,22 @@ public class ItemCalculatorTests
 
     }
 
-    //[Test]
-    //public void CalculateTotalCoinPoints_Test()
-    //{
-    //    // ARRANGE
-    //    var pointCalculator = new ItemCalculator();
-    //    var playerHp = 100;
-    //    var coinPoints = 10;
-    //    var expectedPoints = (100 + 10);
+    [Test]
+    public void CalculateTotalCoinPoints_Test()
+    {
+        // ARRANGE
+        var pointCalculator = new ItemCalculator();
+        var playerHp = 90;
+        var coinPoints = 10;
+        var expectedPoints = (100 + 10);
 
-    //    // ACT
-    //    var points = pointCalculator.CalculateTotalPoints(playerHp, coinPoints);
+        // ACT
+        var points = pointCalculator.CalculateTotalPoints(playerHp, coinPoints);
 
-    //    // ASSERT
-    //    Assert.That(points, Is.EqualTo(expectedPoints));
+        // ASSERT
+        Assert.That(points, Is.EqualTo(expectedPoints));
 
-    //}
+    }
 
     [Test]
     public void PotionPickedUp_Test()
